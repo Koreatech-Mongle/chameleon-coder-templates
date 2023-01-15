869988f74b8c5a170baabec1984cf40aa4b1baf0
refracta <58779799+refracta@users.noreply.github.com>
2023-01-15 13:14:30 +0900

    Fix for test

--- main.tf
+++ main.tf
@@ -26,8 +26,6 @@ resource "coder_agent" "main" {
   os             = "linux"
   startup_script = <<EOT
     #!/bin/bash
-    # execute entrypoint script
-    /bin/bash /usr/sbin/entrypoint
     # install and start code-server
     curl -fsSL https://code-server.dev/install.sh | sh -s -- --version 4.8.3 | tee code-server-install.log
     code-server --auth none --port 13337 | tee code-server-install.log &
